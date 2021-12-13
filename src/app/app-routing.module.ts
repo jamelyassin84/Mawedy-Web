17008dbf062de1490906f844db97fcac132f1701
unknown <jamelyassin84@gmail.com>
2021-12-12 21:49:33 -0800

    Home and Dashboard Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { LoginComponent } from './pages/login/login.component'
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './pages/home/home.component'
+import { AppointmentsComponent } from './dashboard/appointments/appointments.component'
+import { PatientOutreachComponent } from './dashboard/patient-outreach/patient-outreach.component'
 
 const routes: Routes = [
 	{
@@ -16,6 +18,28 @@ const routes: Routes = [
 	{
 		path: 'home',
 		component: HomeComponent,
+		children: [
+			{
+				path: '',
+				pathMatch: 'full',
+				redirectTo: 'dashboard',
+			},
+			{
+				path: 'dashboard',
+				children: [
+					{
+						path: '',
+						pathMatch: 'full',
+						redirectTo: 'appointments',
+					},
+					{ path: 'appointments', component: AppointmentsComponent },
+					{
+						path: 'patient-outreach',
+						component: PatientOutreachComponent,
+					},
+				],
+			},
+		],
 	},
 ]
 
