e66e5dfeecbc2fe7eb59764fbdfe52f5dbc3bb1f
unknown <jamelyassin84@gmail.com>
2021-12-12 21:39:01 -0800

    Login Page UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { LoginComponent } from './pages/login/login.component'
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from './pages/home/home.component'
 
 const routes: Routes = [
 	{
@@ -11,6 +12,10 @@ const routes: Routes = [
 	{
 		path: 'login',
 		component: LoginComponent,
+	},
+	{
+		path: 'home',
+		component: HomeComponent,
 	},
 ]
 
