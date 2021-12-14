a9fe75739322a6d6e250ab5e05d9d766631e1132
unknown <jamelyassin84@gmail.com>
2021-12-14 02:41:36 -0800

    clinic-services

--- src/app/components/utilities/clinic-service-card/clinic-service-card.component.ts
+++ src/app/components/utilities/clinic-service-card/clinic-service-card.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
-	selector: 'clinic-profile-service-card',
+	selector: 'app-service-card',
 	templateUrl: './clinic-service-card.component.html',
 	styleUrls: ['./clinic-service-card.component.scss'],
 })
 export class ClinicServiceCardComponent implements OnInit {
+	@Input() title!: string
+	@Input() description!: string
+	@Input() image!: String
+
 	constructor() {}
 
 	ngOnInit(): void {}
--- src/app/modules/clinic-profile/clinic-profile.module.ts
+++ src/app/modules/clinic-profile/clinic-profile.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { ClinicProfileComponent } from './clinic-profile/clinic-profile.component'
 import { ClinicProfileServicesComponent } from './clinic-profile-services/clinic-profile-services.component'
-import { ClinicServiceCardComponent } from './components/clinic-service-card/clinic-service-card.component'
+import { ClinicServiceCardComponent } from '../../components/utilities/clinic-service-card/clinic-service-card.component'
 import { AddMedicalServicesComponent } from './modals/add-medical-services/add-medical-services.component'
 import { RouterModule } from '@angular/router'
 import { ClinicProfileToolbarComponent } from 'src/app/components/toolbars/clinci-profile-toolbar/clinci-profile-toolbar.component'
@@ -14,12 +14,16 @@ import { TabsComponent } from 'src/app/components/utilities/tabs/tabs.component'
 	declarations: [
 		ClinicProfileComponent,
 		ClinicProfileServicesComponent,
-		ClinicServiceCardComponent,
 		AddMedicalServicesComponent,
 		ClinicProfileToolbarComponent,
 		TabsComponent,
+		ClinicServiceCardComponent,
 	],
 	imports: [CommonModule, DashboardModule, AppRoutingModule, RouterModule],
-	exports: [ClinicProfileToolbarComponent, TabsComponent],
+	exports: [
+		ClinicProfileToolbarComponent,
+		TabsComponent,
+		ClinicServiceCardComponent,
+	],
 })
 export class ClinicProfileModule {}
