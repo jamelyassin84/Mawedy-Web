1f3cc69928497fed5021e5e7487c79a3d841e8ae
Jamel Yassin <jamelyassin84@gmail.com>
2022-01-04 12:52:34 +0400

    responsive to mobile log-in

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,7 +4,14 @@ import { Router } from '@angular/router'
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.scss'],
+	styleUrls: [
+		'./login.component.scss',
+		'./_large.scss',
+		'./_mobile.scss',
+		'./_tabletHQ.scss',
+		'./_tabletsLQ.scss',
+		'./_desktop.scss',
+	],
 })
 export class LoginComponent implements OnInit {
 	constructor(private router: Router) {}
