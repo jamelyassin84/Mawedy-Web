865926bfd72638b4f17a01ff3605d3ce945def57
unknown <jamelyassin84@gmail.com>
2022-01-31 02:22:49 -0800

    add medical service form

--- src/app/components/modal-components/add-medical-services/add-medical-services-modal-body/add-medical-services-modal-body.component.ts
+++ src/app/components/modal-components/add-medical-services/add-medical-services-modal-body/add-medical-services-modal-body.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core'
+import { listAnimation } from './../../../../animations/List.animation'
+import { Component, Input, OnInit } from '@angular/core'
+import { Department } from 'src/app/models/types'
 
 @Component({
 	selector: 'add-medical-services-modal-body',
 	templateUrl: './add-medical-services-modal-body.component.html',
 	styleUrls: ['./add-medical-services-modal-body.component.scss'],
+	animations: [listAnimation],
 })
 export class AddMedicalServicesModalBodyComponent implements OnInit {
 	constructor() {}
 
 	ngOnInit(): void {}
+
+	files!: File[]
+
+	@Input() department!: Department
+
+	trigger(id: string): void {
+		document.getElementById(id)?.click()
+	}
+
+	readFiles(event: any): void {
+		this.files = event.target.files as File[]
+	}
 }
